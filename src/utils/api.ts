db5d53728840aadfd808d1a47b98c57b867feca8
Charles Caleb <charlesofficial9@gmail.com>
2024-11-20 05:13:06 +0100

    update

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://task-management-kcyf.onrender.com/api";
 
 export const getProjects = async () => {
   try {
